50cba0416118dc81cf24e0102ae4aabdfb9b40cd
elliot <elliot@majorana.felk.cvut.cz>
2025-09-29 17:52:11 +0200

    Update compilation script

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-gcc movacg.c -o movavg -lm
+gcc movavg.c -o movavg -lm
+gfortran -o ave ave.f90
+gfortran -o rdf rdf.f90
+chmod +x rdf_pp.sh
 
 echo "export PATH="$PATH:$(pwd)"" >> ~/.bashrc
+
+exit 0
