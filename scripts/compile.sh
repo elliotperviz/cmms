a9e23985f5b39ac1fbfa3a0b23d2cd01a7cb5588
elliot <elliot@majorana.felk.cvut.cz>
2025-10-15 17:56:15 +0200

    Modify compile.sh

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -5,6 +5,14 @@ gfortran -o ave ave.f90
 gfortran -o rdf rdf.f90
 chmod +x rdf_pp.sh
 
-echo 'export PATH=$PATH:'"$(pwd)" >> ~/.bashrc
+# Find script path
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Add script path to PATH
+echo 'export PATH=$PATH:'"$SCRIPT_DIR" >> ~/.bashrc
+
+# Add lammps-tools folder to PATH and set LAMMPS_PYTHON_TOOLS
+echo 'export PATH=$PATH:'"$SCRIPT_DIR/lammps-tools" >> ~/.bashrc
+echo 'export LAMMPS_PYTHON_TOOLS='"$SCRIPT_DIR/lammps-tools/pizza" >> ~/.bashrc
 
 exit 0
