ec76680f1706479c62b03760a822e28cc135d156
elliot <elliot@majorana.felk.cvut.cz>
2025-12-08 17:09:26 +0100

    Update phonon tutorial files

--- ph/2-mos2/pho/mos2-run.sh
+++ ph/2-mos2/pho/mos2-run.sh
@@ -1,32 +1,72 @@
 #!/bin/bash
 
-phonopy --abinit --tolerance=1e-8 -d disp.conf -c prim.in
+list="001 002 003 004"
 
-# Variable definitions
-list=`ls supercell-*.in | grep -oE '[0-9]{3}'`
+ab=abinit
 
-# Prepare displaced supercell calculations
-for i in $list ; do 
-mkdir $i; mv supercell-$i.in $i/ ; cp ab_common $i/
-cd $i; cat supercell-$i.in ab_common > ab.in ; rm supercell-$i.in ab_common; ln -s ../*.psp8 . ; cd ../
+for i in $list; do
+	cd ${i}
+	ln -s ../*.xml .
+
+	mpirun -np 1 $ab ab.in | tee out_paral
+
+	unset np_spkpt npfft npband bandpp ncores
+
+	read np_spkpt npfft npband bandpp ncores < <(
+  	  grep -A 3 -m 1 "np_spkpt" ab.abo | tail -n 1 | awk -F '|' '{print $2, $3, $4, $5, $6}'
+	)
+
+        # Check that all parallelisation parameters were read correctly
+	if [[ -z "$np_spkpt" || -z "$npfft" || -z "$npband" || -z "$bandpp" || -z "$ncores" ]]; then
+    		echo
+    		echo "ERROR: One or more parallelisation parameters (np_spkpt, npfft, npband, bandpp, ncores) are empty."
+    		echo "Check ab.abo file parsing."
+		echo "Current folder: $i"
+    		echo
+    		exit 10
+	fi
+
+	sed -i "/np_spkpt/c\np_spkpt ${np_spkpt}" ab.in
+	sed -i "/npfft/c\npfft ${npfft}" ab.in
+	sed -i "/npband/c\npband ${npband}" ab.in
+	sed -i "/bandpp/c\bandpp ${bandpp}" ab.in
+	sed -i 's/autoparal/#autoparal/' ab.in
+	sed -i 's/max_ncpus/#max_ncpus/' ab.in
+	sed -i 's/#paral_kgb/paral_kgb/' ab.in
+
+	mpir="mpirun -np ${ncores}"
+
+	$mpir $ab ab.in | tee out
+
+	rm -f ERROR
+	chk=`grep "Calculation completed" out`
+	if [ -z "$chk" ] ; then
+        	echo 
+        	echo " ERROR: Calculation not properly completed. Stopping PHONON calculation."
+        	echo
+        	echo " ERROR: Calculation not properly completed. Stopping PHONON calculation." > ERROR
+        	exit 111
+	fi
+
+	chk2=`tail -n 100 out | grep "Calculation completed"`
+	if [ -z "$chk2" ] && [ ! -z "$chk" ] ; then
+        	echo
+        	echo "WARNING: improper termination, but calculation completed. Check output."
+        	echo
+        	echo "WARNING: improper termination, but calculation completed. Check output." > warning
+	fi
+
+	chk1=`grep -m1 nstep out | awk '{print $2}'`
+	chk2=`grep ETOT out |tail -n1 | awk '{print $2}'`
+	if [[ $chk1 = $chk2 ]]; then
+		echo 
+		echo " ERROR: SCF not converged. Stopping PHONON calculation."
+		echo
+		echo " ERROR: SCF not converged. Stopping PHONON calculation." > ERROR
+		exit 222
+	fi
+
+	cd ../
 done
 
-# Run abinit
-for j in $list ; do cd $j; mpirun -np 4 abinit ab.in | tee out ; cd ../ ; done
-
-# Check calculations
-grep -A 1 "Calculation completed" */out
-grep "etot is converged" */out
-
-# Extract forces
-phonopy -f {001..006}/ab.abo
-
-# Post-process band structure
-phonopy --abinit -c prim.in -p band.conf
-
-# Post-process animation and visualise with vsim
-phonopy --abinit -c prim.in -p anime_GM.conf
-v_sim anime.ascii
-
 exit 0
-
---
+++ ph/2-mos2/pho/postproc.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Extract forces
+phonopy --abinit --tolerance1e-8 --fz superc/ab.abo {001..004}/ab.abo
+
+# Post-process band structure
+phonopy --abinit --tolerance=1e-8 -c prim.in -p band.conf
+
+#or
+#$ phonopy --abinit -c prim.in -p -s band.conf
+#$ phonopy-bandplot --gnuplot band.yaml > band.gp
+#and
+#$ gnuplot> plot "band.gp" u 1:2 w l
+
+# Post-process animation and visualise with vsim
+phonopy --abinit --tolerance=1e-8 -c prim.in -p anime_GM.conf
+v_sim anime.ascii
+
+exit 0
+
